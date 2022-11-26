bdc5b9d3f292fe5e247dde0f38b6cd5e592ca77f
Saulius Balčiūnas <sauliunas.balciunas@gmail.com>
2022-11-26 12:46:59 +0200

    ScoreTable and ScoreInput styling updated to be in the same line

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ function App() {
   return (
     <div className="h-96 mx-auto max-w-3xl px-4 py-6 my-10 sm:rounded sm:border sm:border-black text-xs">
       <TeamInput></TeamInput>
-      <ScoreTable></ScoreTable>
-      <ScoreInput></ScoreInput>
+      <div className="sm:flex gap-4 p-2">
+        <ScoreTable></ScoreTable>
+        <ScoreInput></ScoreInput>
+      </div>
     </div>
   );
 }
