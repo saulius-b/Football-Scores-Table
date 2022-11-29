6637c3d5a1f8815e1a19f60912c096419e5b78b4
Saulius Balčiūnas <sauliunas.balciunas@gmail.com>
2022-11-29 22:25:35 +0200

    table data calculations moved to a separate function

--- src/components/ScoreTable.tsx
+++ src/components/ScoreTable.tsx
@@ -1,42 +1,11 @@
 import { useSelector } from "react-redux";
+import { ScoreTableCalculations } from "../features/scoreTableCalculations";
 import { RootState } from "../store/store";
-import { TeamScores } from "../types";
 
 export function ScoreTable() {
-  const allMatchResults = useSelector((state: RootState) => state.addTeam.matchResults);
-
-  //Selecting only the teams that already have played in matches
-  const teamsThatHavePlayed = [...Array.from(new Set(allMatchResults.map((item) => item.team)))];
-
-  const sortedTeams: TeamScores[][] = [];
-
-  teamsThatHavePlayed.forEach((team) => {
-    sortedTeams.push(
-      allMatchResults.filter((item) => {
-        return item.team === team;
-      })
-    );
-  });
-
-  const tableData: TeamScores[] = [];
-
-  sortedTeams.forEach((team) => {
-    const teamResult: TeamScores = {
-      team: team[0].team,
-      played: Object.values(team).reduce((x, { played }) => x + played, 0),
-      win: Object.values(team).reduce((x, { win }) => x + win, 0),
-      draw: Object.values(team).reduce((x, { draw }) => x + draw, 0),
-      lost: Object.values(team).reduce((x, { lost }) => x + lost, 0),
-      points: Object.values(team).reduce((x, { points }) => x + points, 0),
-    };
-    tableData.push(teamResult);
-  });
-
-  tableData.sort((a, b) => {
-    return b.points - a.points || a.team.localeCompare(b.team);
-  });
-
   const headersText: string[] = ["Place", "Team", "Played", "Win", "Draw", "Lost", "Points"];
+  const allMatchResults = useSelector((state: RootState) => state.addTeam.matchResults);
+  const tableData = ScoreTableCalculations(allMatchResults);
 
   const tableHeader = headersText.map((item, index) => {
     return (
--- src/features/scoreTableCalculations.ts
+++ src/features/scoreTableCalculations.ts
@@ -1,1 +1,35 @@
-export function ScoreTableCalculations() {}
+import { TeamScores } from "../types";
+
+export function ScoreTableCalculations(allMatchResults: TeamScores[]) {
+  //Selecting only the teams that already have played in matches
+  const teamsThatHavePlayed = [...Array.from(new Set(allMatchResults.map((item) => item.team)))];
+
+  const sortedTeams: TeamScores[][] = [];
+
+  teamsThatHavePlayed.forEach((team) => {
+    sortedTeams.push(
+      allMatchResults.filter((item) => {
+        return item.team === team;
+      })
+    );
+  });
+
+  const tableData: TeamScores[] = [];
+
+  sortedTeams.forEach((team) => {
+    const teamResult: TeamScores = {
+      team: team[0].team,
+      played: Object.values(team).reduce((x, { played }) => x + played, 0),
+      win: Object.values(team).reduce((x, { win }) => x + win, 0),
+      draw: Object.values(team).reduce((x, { draw }) => x + draw, 0),
+      lost: Object.values(team).reduce((x, { lost }) => x + lost, 0),
+      points: Object.values(team).reduce((x, { points }) => x + points, 0),
+    };
+    tableData.push(teamResult);
+  });
+
+  tableData.sort((a, b) => {
+    return b.points - a.points || a.team.localeCompare(b.team);
+  });
+  return tableData;
+}
