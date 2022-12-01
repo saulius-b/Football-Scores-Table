5611830efebdbe06be145ba2af634784e18bce38
Saulius Balčiūnas <sauliunas.balciunas@gmail.com>
2022-12-01 14:27:16 +0200

    redux state is now saved to local storage with the help of redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store/store";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
