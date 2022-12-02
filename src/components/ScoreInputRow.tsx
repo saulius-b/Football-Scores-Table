b59f4488662606601ca51fe3fa6d44ded35732a0
Saulius Balčiūnas <sauliunas.balciunas@gmail.com>
2022-12-02 08:42:04 +0200

    ScoreInputRow onchange logic moved to handleTeamChange functions

--- src/components/ScoreInputRow.tsx
+++ src/components/ScoreInputRow.tsx
@@ -8,6 +8,27 @@ export function ScoreInputRow(props: { item: string[]; index: number; team1Score
   const [team1Value, setTeam1Value] = useState<number>(props.team1Score === undefined ? 0 : props.team1Score[0]);
   const [team2Value, setTeam2Value] = useState<number>(props.team2Score === undefined ? 0 : props.team2Score[0]);
 
+  function handleTeam1Change(event: React.ChangeEvent<HTMLInputElement>) {
+    setTeam1Value(parseInt(event.target.value));
+    dispatch(
+      addMatch({
+        team: props.item[0],
+        pointsScored: parseInt(event.target.value),
+        matchId: props.index,
+      })
+    );
+  }
+  function handleTeam2Change(event: React.ChangeEvent<HTMLInputElement>) {
+    setTeam2Value(parseInt(event.target.value));
+    dispatch(
+      addMatch({
+        team: props.item[1],
+        pointsScored: parseInt(event.target.value),
+        matchId: props.index,
+      })
+    );
+  }
+
   return (
     <div className="flex justify-center">
       <div className="flex w-24 gap-2 justify-end">
@@ -17,14 +38,7 @@ export function ScoreInputRow(props: { item: string[]; index: number; team1Score
           type={"number"}
           defaultValue={team1Value}
           onChange={(event) => {
-            setTeam1Value(parseInt(event.target.value));
-            dispatch(
-              addMatch({
-                team: props.item[0],
-                pointsScored: event.target.value === "" ? 0 : parseInt(event.target.value),
-                matchId: props.index,
-              })
-            );
+            handleTeam1Change(event);
           }}
         ></input>
       </div>
@@ -35,14 +49,7 @@ export function ScoreInputRow(props: { item: string[]; index: number; team1Score
           type={"number"}
           defaultValue={team2Value}
           onChange={(event) => {
-            setTeam2Value(parseInt(event.target.value));
-            dispatch(
-              addMatch({
-                team: props.item[1],
-                pointsScored: event.target.value === "" ? 0 : parseInt(event.target.value),
-                matchId: props.index,
-              })
-            );
+            handleTeam2Change(event);
           }}
         ></input>
         <div>{props.item[1]}</div>
